bef21780ea1727385bcfdc7ee67824adc173d862
b1mind <brent@1mindsolutions.com>
2021-10-05 18:11:26 -0600

    🐣 Clean up and adding assets.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,7 @@
 <script>
-  import Header from '$lib/Header.svelte';
-  import Footer from '$lib/Footer.svelte';
+  import Header from '$lib/Header.svelte'
+  import Footer from '$lib/Footer.svelte'
+  import '../app.scss'
 </script>
 
 <Header />
