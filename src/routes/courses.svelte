7eaaaf3b82fb7b4b28b2bfb879cdc16f61865b73
b1mind <brent@1mindsolutions.com>
2021-10-05 22:42:33 -0600

    ➕ started course pages

---
+++ src/routes/courses.svelte
@@ -0,0 +1,1 @@
+Courses Home page test
---
+++ src/routes/courses/[slug].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+  //todo use import to get page
+  export async function load({ fetch, page }) {
+    let slugTitle = page.params.slug
+    return { props: { slugTitle, page } }
+  }
+</script>
+
+<script>
+  export let slugTitle
+</script>
+
+{slugTitle}
+
+This would be a course category
+<!-- //Todo filter with slug -->
+<p>data.filter(slugTitle)</p>
