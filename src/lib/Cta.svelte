6a9fb565a63cbe5b0a545fd711efe4566946c156
b1mind <brent@1mindsolutions.com>
2021-10-06 21:07:19 -0600

    ➕start of CTA sections

---
+++ src/lib/Cta.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let reverse
+</script>
+
+<section>
+  <div class="flex" class:reverse>
+    <div class="text">
+      <slot name="text" />
+    </div>
+    <div class="img">
+      <slot name="img" />
+    </div>
+  </div>
+</section>
+
+<style lang="scss">
+  @use './scss/vars' as *;
+
+  .flex {
+    justify-content: space-evenly;
+    align-items: center;
+    flex-wrap: wrap;
+    & > * {
+      max-width: 50%;
+    }
+  }
+
+  .img {
+    border-radius: 2.25rem;
+    box-shadow: var(--bs);
+    overflow: hidden;
+  }
+
+  //todo alt cta
+  .reverse {
+    flex-wrap: wrap-reverse;
+    flex-direction: row-reverse;
+  }
+</style>
--- src/routes/courses.svelte
+++ src/routes/courses.svelte
@@ -1,6 +1,23 @@
 <script>
   import Footer from '$lib/Footer.svelte'
+  import Cta from '$lib/Cta.svelte'
 </script>
 
-Courses Home page test
+<section class="container">
+  <h1>Courses Home page test</h1>
+</section>
+
 <Footer />
+
+<Cta>
+  <div slot="text">Some text</div>
+  <img class="small" slot="img" src="/img/yarn.png" alt="yarn" />
+</Cta>
+
+<Cta reverse="true">
+  <div slot="text">Some text</div>
+  <img class="big" slot="img" src="/img/profile-pic.png" alt="yarn" />
+</Cta>
+
+<style lang="scss">
+</style>
