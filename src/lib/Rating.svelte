a9052923b27d1c219ea726d670dbd46a0cfb962c
b1mind <brent@1mindsolutions.com>
2021-10-08 10:55:08 -0600

    🧪testing: rating svg outline/stroke outline

--- src/lib/Rating.svelte
+++ src/lib/Rating.svelte
@@ -1,39 +1,67 @@
 <script>
   export let stars
   export let half
+  const outlines = 5
 </script>
 
-{#each Array(stars) as _}
-  <svg
-    width="16"
-    height="16"
-    viewBox="0 0 16 16"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M8.00001 1L5.72501 5.61L0.640015 6.345L4.32001 9.935L3.45001 15L8.00001 12.61L12.55 15L11.68 9.935L15.36 6.35L10.275 5.61L8.00001 1Z"
-      fill="black"
-    />
-  </svg>
-{/each}
+<div class="outline">
+  {#each Array(outlines) as _}
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      ><path
+        d="M8.00001 1L5.72501 5.61L0.640015 6.345L4.32001 9.935L3.45001 15L8.00001 12.61L12.55 15L11.68 9.935L15.36 6.35L10.275 5.61L8.00001 1Z"
+        fill="white"
+        stroke="black"
+      />
+    </svg>
+  {/each}
+  <div class="rating">
+    {#each Array(stars) as _}
+      <svg
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M8.00001 1L5.72501 5.61L0.640015 6.345L4.32001 9.935L3.45001 15L8.00001 12.61L12.55 15L11.68 9.935L15.36 6.35L10.275 5.61L8.00001 1Z"
+          fill="black"
+        />
+      </svg>
+    {/each}
 
-{#if !half}
-  <svg
-    width="16"
-    height="16"
-    viewBox="4 0 16 16"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M5.72502 5.61L0.640015 6.35L4.32002 9.935L3.45001 15L8.00002 12.61V1L5.72502 5.61Z"
-      fill="black"
-    />
-  </svg>
-{/if}
+    {#if !half}
+      <svg
+        width="16"
+        height="16"
+        viewBox="4 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M5.72502 5.61L0.640015 6.35L4.32002 9.935L3.45001 15L8.00002 12.61V1L5.72502 5.61Z"
+          fill="black"
+        />
+      </svg>
+    {/if}
+  </div>
+</div>
 
 <style lang="scss">
+  .outline {
+    position: relative;
+  }
+
+  .rating {
+    position: absolute;
+    top: 0;
+  }
+
   svg {
     overflow: visible;
   }
