cd146a26430c66f383ee09e0f387837b7cf3405e
b1mind <brent@1mindsolutions.com>
2021-10-08 13:11:48 -0600

    🔧fix: stars alignment

--- src/lib/Rating.svelte
+++ src/lib/Rating.svelte
@@ -21,9 +21,9 @@
 
 {#if !half}
   <svg
-    width="16"
-    height="16"
-    viewBox="4 0 16 16"
+    width="14"
+    height="14"
+    viewBox="4 1 16 16"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
