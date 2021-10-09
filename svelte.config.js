0fee2c6247ccfba738666d8855a439bce791e6db
b1mind <brent@1mindsolutions.com>
2021-10-08 21:03:17 -0600

    📦 Vite: compress for images

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
 /** @type {import('@sveltejs/kit').Config} */
-import vercel from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel'
+import preprocess from 'svelte-preprocess'
+import compress from 'vite-plugin-compress'
 
 export default {
   kit: {
-    adapter: vercel()
+    adapter: vercel(),
+    vite: () => ({
+      plugins: [compress]
+    })
   },
   preprocess: preprocess({})
-};
+}
